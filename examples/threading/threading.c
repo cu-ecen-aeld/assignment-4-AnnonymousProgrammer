a4ba9278573605bc257519ceb418b52e7b47eb0f
Luki <lukas.rosenbauer@hotmail.de>
2023-08-23 16:03:37 +0200

    add impl

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,19 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    if(thread_param != NULL){
+    	    struct thread_data * thread_func_args = (struct thread_data *) thread_param;
+    	    thread_func_args->thread_complete_success = false;
+	    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+	    //printf("trying to acquire mutex");
+	    pthread_mutex_lock(thread_func_args->mutex);
+	    usleep(thread_func_args->wait_to_release_ms * 1000);
+	    //printf("trying to unlock mutex");
+	    pthread_mutex_unlock(thread_func_args->mutex);
+	    thread_func_args->thread_complete_success = true;
+    }
+    
+    
     return thread_param;
 }
 
@@ -28,6 +41,40 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+     
+     struct thread_data * ptr = (struct thread_data *)  malloc(sizeof(struct thread_data));
+     if(ptr != NULL){
+     	ptr->wait_to_obtain_ms = wait_to_obtain_ms;
+     	ptr->wait_to_release_ms = wait_to_release_ms;
+     	ptr->mutex = mutex;
+     	pthread_t my_thread;
+     	int rc1 = pthread_create(&my_thread, NULL, threadfunc, (void *) ptr);
+     	if(rc1 != 0){
+     		return false;
+     	}
+     	//int rc2 = pthread_join(my_thread, NULL);
+     	//if(rc2 != 0){
+     	//	return false;
+     	//}
+     	//if(ptr->thread_complete_success == true){
+     	*thread = my_thread;
+     		
+     	//}
+     	//free(ptr);
+     	return true;
+     	
+     }
+     
+     
     return false;
 }
+
+/**
+int main(){
+	pthread_t t;
+	pthread_mutex_t mutex;
+	pthread_mutex_init(&mutex, NULL);
+	start_thread_obtaining_mutex(&t, &mutex, 10, 10);
+}
+**/
 
